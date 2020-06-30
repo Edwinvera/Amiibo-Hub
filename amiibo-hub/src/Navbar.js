4e2125b05b29d8092430ac983286ae2007b96e49
Edwin Vera <edwinvera@Edwins-MacBook-Air.local>
2020-06-30 13:49:13 -0400

    Here we go

---
+++ amiibo-hub/src/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>Amiibo Hub</h1>
+      <a href="https://www.nintendo.com/amiibo/buynow/">Buy</a>
+    </nav>
+  )
+}
